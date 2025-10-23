e4a31f7a551c617d8ca96e68d79ebee5dec47875
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-23 21:40:45 +0300

    sequence sum calculator with validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum = 0;
+
+    loop {
+        let mut input: String = String::new();
+
+        if io::stdin().read_line(&mut input).is_err() {
+            println!("NaN");
+            return;
+        }
+
+        let input = input.trim();
+
+        match input.parse::<i32>() {
+            Ok(num) => {
+                if num == -1 {
+                    break;
+                }
+
+                if num > 0 {
+                    sum += num;
+                } else {
+                    println!("NaN");
+                    return;
+                }
+            }
+
+            Err(_) => {
+                println!("NaN");
+                return;
+            }
+        }
+    }
+
+    println!("{}", sum);
 }
